c13c11e8fd99378cb5045c5acfc9bcb607f4be0a
arifsubroto <arifsubroto8@gmail.com>
2017-04-08 15:38:18 +0700

    Initial commit

---
+++ app/src/main/java/com/arifsubroto/baseelevencalc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.arifsubroto.baseelevencalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
